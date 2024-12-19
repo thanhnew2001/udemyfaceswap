7ae1bacc9cbd1872ad56cfb9fab7cc8c9086f800
thanhnew2001 <69960815+thanhnew2001@users.noreply.github.com>
2024-12-19 22:00:39 +1100

    add other albums

--- app.py
+++ app.py
@@ -1,16 +1,12 @@
 import os
-import replicate
 from flask import Flask, render_template, request, jsonify, send_from_directory
 from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
 
-# Cấu hình thư mục lưu ảnh upload
-UPLOAD_FOLDER = 'uploads'
-ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
-
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024  # Giới hạn dung lượng ảnh upload tối đa là 16MB
+# Cấu hình thư mục lưu ảnh upload và ảnh mẫu
+IMAGE_FOLDER = 'static'
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'webp'}
 
 # Kiểm tra định dạng file
 def allowed_file(filename):
@@ -19,12 +15,22 @@ def allowed_file(filename):
 # Route trả về file index.html
 @app.route('/')
 def index():
-    # Lấy danh sách ảnh mẫu từ thư mục 'static/images'
-    image_files = os.listdir(os.path.join(app.static_folder, 'images'))
-    image_files = [f'/static/images/{file}' for file in image_files if allowed_file(file)]
-    
-    # Render template index.html và truyền dữ liệu danh sách ảnh mẫu
-    return render_template('index.html', image_files=image_files)
+    # Khởi tạo dictionary để lưu album và ảnh
+    albums = {}
+
+    # Duyệt qua các thư mục (album) trong thư mục 'static/images'
+    for album_name in os.listdir(IMAGE_FOLDER):
+        album_path = os.path.join(IMAGE_FOLDER, album_name)
+        if os.path.isdir(album_path):  # Kiểm tra xem có phải là thư mục (album)
+            album_images = [
+                f"{album_name}/{f}" for f in os.listdir(album_path)
+                if allowed_file(f)
+            ]
+            albums[album_name] = album_images  # Thêm album và ảnh của album
+        
+    print(albums)
+
+    return render_template('index.html', albums=albums)
 
 # API để xử lý upload ảnh và gọi Replicate API
 @app.route('/upload', methods=['POST'])
@@ -73,6 +79,6 @@ def uploaded_file(filename):
     return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
 
 if __name__ == '__main__':
-    if not os.path.exists(UPLOAD_FOLDER):
-        os.makedirs(UPLOAD_FOLDER)
+    if not os.path.exists(IMAGE_FOLDER):
+        os.makedirs(IMAGE_FOLDER)
     app.run(debug=True)
