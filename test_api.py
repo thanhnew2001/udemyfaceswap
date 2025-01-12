748068f215b9653b429ae675035642f6b8780eab
thanhnew2001 <thanh.hispvietnam@gmail.com>
2025-01-13 09:23:21 +1100

    add lotus and tet2025

--- test_api.py
+++
@@ -1,9 +0,0 @@
-    <!-- Favicon -->
-    <link rel="icon" type="image/png" href="https://photo.sydneyonlineuniversity.org/static/favicon.png">
-
-    <!-- Open Graph Meta Tags for Social Sharing -->
-    <meta property="og:title" content="StudioHer">
-    <meta property="og:description" content="Tự tạo ảnh sống ảo, phông bạt miễn phí trong 3 giây. Studio sống ảo. Hình đẹp lấy ngay, thợ chụp khỏi hẹn ngày">
-    <meta property="og:image" content="https://photo.sydneyonlineuniversity.org/static/thumbnail.png">
-    <meta property="og:url" content="https://photo.sydneyonlineuniversity.org/">
-    <meta property="og:type" content="website">
